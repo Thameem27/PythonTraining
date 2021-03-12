44dc27f5444716e8adafdeaf7d1b25d30001889b
Thameem27 <79091540+Thameem27@users.noreply.github.com>
2021-03-12 11:55:25 +0530

    Add files via upload

---
+++ Day14/ninjasdjango/ninjas/admin.py
@@ -0,0 +1,20 @@
+from django.contrib import admin
+from .models import Developer,Skill
+
+class SkillInLine(admin.StackedInline):
+    model = Skill
+    extra = 1
+
+class DeveloperAdmin(admin.ModelAdmin):
+    fieldsets = [
+        ('Name', {'fields':['name']}),
+        ('Country', {'fields':['country'],}),
+        ('Experience', {'fields':['experience']}),
+    ]
+    inlines = [SkillInLine]
+
+admin.site.register(Developer, DeveloperAdmin)
+
+# Register your models here.
+# admin.site.register(Developer)
+# admin.site.register(Skill)
